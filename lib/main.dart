1d3016dbd556384cb72aec99e59bff0861e72fd2
Mukund Solanki <mukundsolanki665@gmail.com>
2023-06-21 15:25:25 +0530

    Changes Commited 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +18,19 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               CircleAvatar(
                 radius: 100.0,
-                backgroundImage: AssetImage('images/Mukund.jpg'),
+                backgroundImage: AssetImage('images/doge.jpg'),
               ),
-              Text(
-                '<Mukund Solanki/>',
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
-                  color: Colors.black,
+              Padding(
+                padding: EdgeInsets.only(top: 20.0), // Adjust the spacing as needed
+                child: Text(
+                  '<Mukund Solanki/>',
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                    color: Colors.black,
+                  ),
                 ),
               ),
               Text(
@@ -54,7 +57,7 @@ class MyApp extends StatelessWidget {
                         '+91 7440399548',
                         style: TextStyle(
                             color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                            // fontWeight: FontWeight.bold,
                             letterSpacing: 1.0,
                             fontSize: 20.0),
                       )
@@ -80,7 +83,28 @@ class MyApp extends StatelessWidget {
                         fontSize: 18.0),
                   ),
                 ),
-              )
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.location_pin,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'India',
+                        style: TextStyle(
+                            color: Colors.black,
+                            // fontWeight: FontWeight.bold,
+                            letterSpacing: 1.0,
+                            fontSize: 20.0),
+                      )
+                    )
+                  ),
             ],
           ),
         ),
